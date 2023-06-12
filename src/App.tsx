476c1b16c1f3c8300102e10f5d04780f4bc06941
ALTA-FE14-Robbi <robbiromadhoni@gmail.com>
2023-06-12 09:37:06 +0700

    initial project (perbaiki daisyui)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
