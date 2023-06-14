d1321b107206af287f74fce79c02240ad31ebb8d
ALTA-FE14-Robbi <robbiromadhoni@gmail.com>
2023-06-14 17:24:01 +0700

    memperbarui trip page (blm pop up)

--- src/page/TripPages.tsx
+++ src/page/TripPages.tsx
@@ -14,15 +14,19 @@ const TripPage = () => {
   };
 
   return (
+    // <div className="w-screen h-screen top-0 overflow-x-hidden z-0">
     <div className="w-4/5 h-fit top-0 overflow-x-hidden z-0">
       <Navbar />
       <div className="flex justify-center w-full mt-16">
         <div className="w-4/5 h-fit bg-white drop-shadow-lg rounded-lg my-10 px-16 py-5 border-2 border-primary">
           <p className="mt-3 font-bold text-3xl">Trip</p>
           <hr className="border border-primary my-8" />
-          <p className="mb-3">Villa Master Lorem</p>
-          <div className="flex flex-row w-fit gap-5 mb-3">
-            {/* <DatePicker
+          
+          <div className="flex flex-row mb-5">
+            <div className="basis-1/2 pr-8">
+              <p className="mb-3">Villa Master Lorem</p>
+              <div className="flex flex-row w-fit gap-5 mb-3">
+                {/* <DatePicker
               selected={DateCheckin}
               onChange={handleDateCheckin}
               dateFormat="dd/MM/yyyy"
@@ -36,13 +40,31 @@ const TripPage = () => {
               placeholderText="Tanggal Check-out"
               className="border border-primary rounded px-4 py-2 focus:outline-none focus:border-blue-500"
             /> */}
+              </div>
+              <p className="mb-3">Rp199.000 x 2 malam</p>
+              <p className="mb-3 font-bold">Total Rp398.000</p>
+            </div>
+            <div className="basis-1/2 pl-8">
+              <button className="btn btn-ghost bg-primary text-white">
+                Tulis Review
+              </button>
+            </div>
           </div>
-          <p className="mb-3">Rp199.000 x 2 malam</p>
-          <p className="mb-3 font-bold">Total Rp398.000</p>
-          <hr className="border border-primary my-8" />
-          <p className="mb-3">Villa Master Lorem</p>
-          <div className="flex flex-row w-fit gap-5 mb-3">
-            {/* <DatePicker
+          
+          <div className="flex flex-row">
+            <div className="basis-1/2 pr-8">
+            <hr className="border border-primary my-8" />
+            </div>
+            <div className="basis-1/2 pl-8">
+            <hr className="border border-primary my-8" />
+            </div>
+          </div>
+
+          <div className="flex flex-row mb-5">
+            <div className="basis-1/2 pr-8">
+              <p className="mb-3">Villa Master Lorem</p>
+              <div className="flex flex-row w-fit gap-5 mb-3">
+                {/* <DatePicker
               selected={DateCheckin}
               onChange={handleDateCheckin}
               dateFormat="dd/MM/yyyy"
@@ -56,10 +78,17 @@ const TripPage = () => {
               placeholderText="Tanggal Check-out"
               className="border border-primary rounded px-4 py-2 focus:outline-none focus:border-blue-500"
             /> */}
+              </div>
+              <p className="mb-3">Rp199.000 x 2 malam</p>
+              <p className="mb-3 font-bold">Total Rp398.000</p>
+            </div>
+            <div className="basis-1/2 pl-8 items-end">
+              <button className="btn btn-ghost bg-primary text-white">
+                Tulis Review
+              </button>
+            </div>
           </div>
-          <p className="mb-3">Rp199.000 x 2 malam</p>
-          <p className="mb-3 font-bold">Total Rp398.000</p>
-        </div>
+          </div>
       </div>
     </div>
   );
