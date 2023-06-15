34003d2dc40bc008b18fb9f7783fc16510ed74aa
ALTA-FE14-Robbi <robbiromadhoni@gmail.com>
2023-06-15 18:38:26 +0700

    fix error

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,22 +1,23 @@
-import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
-import React, { FC , useState} from "react";
-import { Link } from "react-router-dom";
+import React, { FC, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+
 interface NavbarProps {
   children?: React.ReactNode;
 }
 
 const Navbar: FC<NavbarProps> = ({ children }) => {
   const navigate = useNavigate();
-  const [cookies,setCookie, removeCookie] = useCookies();
-  const [toggle, setToggle] = useState<boolean>(false);
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const [toggle, setToggle] = useState<boolean>(cookies.toggle === "true");
 
-  const SetToggle = () =>{
-    setCookie('toggle' , !toggle)
-    setToggle(!toggle)
-  }
+  const SetToggle = () => {
+    const newToggle = !toggle;
+    setCookie("toggle", newToggle.toString());
+    setToggle(newToggle);
+  };
 
   const Logout = () => {
     Swal.fire({
@@ -26,112 +27,113 @@ const Navbar: FC<NavbarProps> = ({ children }) => {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Logout ",
+      confirmButtonText: "Logout",
     }).then((result) => {
       if (result.isConfirmed) {
         removeCookie("token", {});
         removeCookie("role", {});
-        Swal.fire("Logout!", "Anda Telah Logout", "success");
-        navigate("/");
+        Swal.fire("Logout!", "Anda Telah Logout", "success").then(() => {
+          navigate("/");
+        });
       }
+    });
+  };
+
   return (
-    <>
-      <div className="absolute w-full h-screen top-0 overflow-x-hidden">
-        <div className="navbar bg-primary text-white shadow-xl sticky top-0 z-20">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
+    <div className="absolute w-full h-screen top-0 overflow-x-hidden">
+      <div className="navbar bg-primary text-white shadow-xl sticky top-0 z-20">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <a>Saved</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl hover:text-white">
+            AirBnB
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/" className="text-white">
+                Home
+              </Link>
+            </li>
+            <li>
+              <a className="text-white">Saved</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="mr-3" onClick={SetToggle}>
+            <label tabIndex={0} className="btn btn-ghost btn-circle text-2xl">
+              <div className="indicator">
+                <span>{toggle ? <BsFillSunFill /> : <BsFillMoonFill />}</span>
+              </div>
+            </label>
+          </div>
+          {cookies?.role === "user" && cookies?.hoosting ? (
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    src="https://static.vecteezy.com/system/resources/previews/007/296/443/original/user-icon-person-icon-client-symbol-profile-icon-vector.jpg"
+                    alt="Profile"
                   />
-                </svg>
+                </div>
               </label>
               <ul
                 tabIndex={0}
                 className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <Link to="/">
-                  <a>Home</a>
-                </Link>
                 <li>
-                  <a>Saved</a>
+                  <Link to="/profil" className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </Link>
+                </li>
+                <li>
+                  <a onClick={Logout}>Logout</a>
                 </li>
               </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-xl hover:text-white">
-              AirBnB
-            </a>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <Link to="/" className="text-white">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <a className="text-white">Saved</a>
-              </li>
-            </ul>
-          </div>
-          <div className="navbar-end">
-            <div className="mr-3" onClick={SetToggle}>
-              <label
-                tabIndex={0}
-                className="btn btn-ghost btn-circle text-2xl "
-              >
-                <div className="indicator">
-                  <span>
-                    {cookies.toggle === "true"? <BsFillSunFill/> : <BsFillMoonFill />}
-                  </span>
-
-                </div>
-              </label>
-            </div>
-            {cookies?.role == "user" && "hoosting" ? (
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                  <div className="w-10 rounded-full">
-                    <img src="https://static.vecteezy.com/system/resources/previews/007/296/443/original/user-icon-person-icon-client-symbol-profile-icon-vector.jpg" />
-                  </div>
-                </label>
-                <ul
-                  tabIndex={0}
-                  className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-                >
-                  <li>
-                    <Link to="/profil" className="justify-between">
-                      Profile
-                      <span className="badge">New</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <a onClick={Logout}>Logout</a>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <Link to="/login">
-                <button className="btn btn-ghost bg-white text-primary hover:bg-opacity-10 hover:text-black ml-5 mr-5 rounded-xl">
-                  Login
-                </button>
-              </Link>
-            )}
-          </div>
+          ) : (
+            <Link to="/login">
+              <button className="btn btn-ghost bg-white text-primary hover:bg-opacity-10 hover:text-black ml-5 mr-5 rounded-xl">
+                Login
+              </button>
+            </Link>
+          )}
         </div>
-        <div>{children}</div>
       </div>
-    </>
+      <div>{children}</div>
+    </div>
   );
 };
 
--- src/page/UserprofilPage.tsx
+++ src/page/UserprofilPage.tsx
@@ -8,57 +8,36 @@ import { useCookies } from "react-cookie";
 
 const UserprofilPage = () => {
   const [cookies] = useCookies();
-  const [listing, setlisting] = useState<string>("");
+  const [listing, setListing] = useState<string>("");
   const [popUPUpgrade, setPopUpUpgrade] = useState(false);
 
-  const HandleListing = () => {
-    setlisting("listing");
-  };
-  const HandleTrip = () => {
-    setlisting("trip");
+  const handleListing = () => {
+    setListing("listing");
   };
 
-  const HandlePopUpUpgrade = () => {
+  const handleTrip = () => {
+    setListing("trip");
+  };
+
+  const handlePopUpUpgrade = () => {
     setPopUpUpgrade(!popUPUpgrade);
   };
 
-    return (
-        <div className='w-screen h-screen top-0 overflow-x-hidden z-0'>
-            <Navbar >
-                <div className='flex justify-center h-fit'>
-                    <div className='flex justify-center w-1/4 h-screen shadow-2xl p-4 sticky top-5'>
-                        <div className='p-10'>
-                            <div className="avatar">
-                                <div className="w-52 rounded-full flex justify-center ml-3">
-                                    <img src="https://static.vecteezy.com/system/resources/previews/007/296/443/original/user-icon-person-icon-client-symbol-profile-icon-vector.jpg" />
-                                </div>
-                            </div>
-                            <div className="mt-3 mb-3 border-t-4 border-primary"></div>
-                            <div className='flex justify-center text-3xl font-bold hover:cursor-pointer' onClick={() => setlisting("")}>
-                                <p>John Doe</p>
-                            </div>
-                            <div className='mt-5 font-bold'>
-                                {cookies.role === "user" ?
-                                    <div className="tooltip w-full" data-tip="Upgrade Acount To Listing your Homestay">
-                                        <button className="btn btn-warning mb-5 w-full text-white" onClick={HandlePopUpUpgrade}>Upgrade Acount</button>
-                                    </div>
-                                    :
-                                    <button className="btn btn-warning mb-5 w-full text-white" onClick={HandleListing}>Listing List</button>}
-                                <button className="btn btn-warning mb-5 w-full text-white" onClick={HandleTrip}>Trip History</button>
-
-                            </div>
-                        </div>
-
-                    </div>
-                    {listing === "" ? <Profil /> : listing === "listing" ? <ListingList /> : <TripPage />}
-
-
+  return (
+    <div className="w-screen h-screen top-0 overflow-x-hidden z-0">
+      <Navbar>
+        <div className="flex justify-center h-fit">
+          <div className="flex justify-center w-1/4 h-screen shadow-2xl p-4 sticky top-5">
+            <div className="p-10">
+              <div className="avatar">
+                <div className="w-52 rounded-full flex justify-center ml-3">
+                  <img src="https://static.vecteezy.com/system/resources/previews/007/296/443/original/user-icon-person-icon-client-symbol-profile-icon-vector.jpg" />
                 </div>
               </div>
               <div className="mt-3 mb-3 border-t-4 border-primary"></div>
               <div
                 className="flex justify-center text-3xl font-bold hover:cursor-pointer"
-                onClick={() => setlisting("")}
+                onClick={() => setListing("")}
               >
                 <p>John Doe</p>
               </div>
@@ -69,23 +48,23 @@ const UserprofilPage = () => {
                     data-tip="Upgrade Acount To Listing your Homestay"
                   >
                     <button
-                      className="btn btn-ghost bg-primary mb-5 w-full text-white"
-                      onClick={HandlePopUpUpgrade}
+                      className="btn btn-warning mb-5 w-full text-white"
+                      onClick={handlePopUpUpgrade}
                     >
-                      Upgrade Acount
+                      Upgrade Account
                     </button>
                   </div>
                 ) : (
                   <button
-                    className="btn btn-ghost bg-primary mb-5 w-full text-white"
-                    onClick={HandleListing}
+                    className="btn btn-warning mb-5 w-full text-white"
+                    onClick={handleListing}
                   >
                     Listing List
                   </button>
                 )}
                 <button
-                  className="btn btn-ghost bg-primary mb-5 w-full text-white"
-                  onClick={HandleTrip}
+                  className="btn btn-warning mb-5 w-full text-white"
+                  onClick={handleTrip}
                 >
                   Trip History
                 </button>
@@ -101,7 +80,6 @@ const UserprofilPage = () => {
           )}
         </div>
       </Navbar>
-
       {popUPUpgrade && <PopupUpgrade setShowPopup={setPopUpUpgrade} />}
     </div>
   );
